0e0869e381ec01130bf6a42e0755f9b8caf1f11e
Logstop_coder-PC <Logstop_coder-PC@192.168.0.107>
2022-04-16 10:28:52 +0530

    commit new files added

---
+++ main.c
@@ -0,0 +1,14 @@
+/* 
+ * File:   main.c
+ * Author: Logstop_coder-PC
+ *
+ * Created on April 16, 2022, 10:25 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    ;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=demoX.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/demoX.X.production.hex
